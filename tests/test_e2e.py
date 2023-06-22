05c7bcbc6815ba7331d2e48e71696aa35bd60d14
trevharri <trevor.harri94@gmail.com>
2023-06-22 16:11:34 +0200

    added logging functionality

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -6,14 +6,16 @@ class TestOne(BaseClass):
 
     def test_e2e(self):
         product = "Blackberry"
-
+        log = self.getLogger()
         home_page = HomePage(self.driver)
 
         checkout_page = home_page.shopItems()
+        log.info("Getting all card titles")
         products = checkout_page.getCardTitles()
         products_text_list = [product.text for product in products]
         if product in products_text_list:
             index = products_text_list.index(product)
+            log.info("Matching product found: {}".format(product))
             button_css_path = f"div.col-lg-9 app-card:nth-child({index + 1}) button"
         checkout_page.getCardButton(button_css_path).click()
         # print(self.driver.find_element(By.XPATH, "//a[@class='nav-link btn btn-primary']").text)
@@ -21,6 +23,7 @@ class TestOne(BaseClass):
         confirm_page = checkout_page.getCheckoutButton()
 
         confirm_page.getCountryInput().send_keys('Spain')
+        log.info("Entering country name: Spain")
         self.verifyLinkPresense('Spain')
         confirm_page.getCountryOption().click()
         confirm_page.getPurchaseButton().click()
--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -1,11 +1,26 @@
+import inspect
+import logging
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.select import Select
 
+
 @pytest.mark.usefixtures('setup')
 class BaseClass:
+    def getLogger(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
+
+        logger.addHandler(fileHandler)  # filehandler object
+
+        logger.setLevel(logging.DEBUG)  # only info and up will print
+        return logger
 
     def verifyLinkPresense(self, link_txt):
         wait = WebDriverWait(self.driver, 10)
